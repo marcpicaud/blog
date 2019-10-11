5768a285dc15e7a79dc1535808d6e30f7e65ff0a
Marc Picaud <picaud.marc@gmail.com>
2019-10-11 03:35:04 +0200

    Update site's metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Dos pesos`,
+    author: `Marc Picaud`,
+    description: `My 2 pesos on life.`,
+    siteUrl: `https://blog.marcpicaud.dev`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `marcpicaud`,
     },
   },
   plugins: [
